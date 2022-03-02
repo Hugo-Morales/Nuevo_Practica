d441ee56baa3c553e25a0f2ceedd10a1c8869de3
ethanialq <ethanialq@gmail.com>
2022-03-01 18:03:53 -0600

    clase persona

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //// Luciano
+
+class Persona {
+    constructor(nombre) {
+        this.nombre = nombre
+    }
+
+    saludar() {
+        return "Hola, soy"+ this.nombre
+    }
+}
+
+const Ethan = new Persona('Ethan')
+Ethan.saludar()
